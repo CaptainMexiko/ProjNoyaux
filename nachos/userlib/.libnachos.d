f38f43530ff971e1ecd277d8efe9445639103f6f
CaptainMexiko <didotgwendal@gmail.com>
2018-02-02 09:11:29 +0100

    Ajout de cross-mips pour pouvoir bosser chez soi

---
+++ nachos/userlib/.libnachos.d
@@ -0,0 +1,5 @@
+libnachos.o .libnachos.d : libnachos.c libnachos.h ../userlib/syscall.h \
+ ../kernel/copyright.h \
+ /home/gwendal/Boulot/M1-SSR/NOY/ProjNoyaux/nachos/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdarg.h \
+ /home/gwendal/Boulot/M1-SSR/NOY/ProjNoyaux/nachos/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdint.h \
+ /home/gwendal/Boulot/M1-SSR/NOY/ProjNoyaux/nachos/cross-mips/bin/../lib/gcc/mips/4.5.1/../../../../mips/include/stdint.h
